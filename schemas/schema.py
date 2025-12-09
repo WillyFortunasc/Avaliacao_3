8666cbeb562bc8ff12b209ee1d316f46a67c6c66
Willy Fortuna <willyfortunasc.unb@gmail.com>
2025-12-09 12:07:28 -0300

    atualização

--- schemas/schema.py
+++ schemas/schema.py
@@ -2,22 +2,30 @@ from pydantic import BaseModel
 from datetime import date
 from typing import List, Optional
 
+
 class ItemPedidoSchema(BaseModel):
     produto: str
     quantidade: int
     preco: float
 
+
 class PedidoCreateSchema(BaseModel):
     cliente: str
     itens: List[ItemPedidoSchema]
+
 
 class PedidoUpdateSchema(BaseModel):
     cliente: Optional[str] = None
     data_pedido: Optional[date] = None
 
+
 class ItemPedidoOutSchema(ItemPedidoSchema):
     id: int
     pedido_id: int
+
+    class Config:
+        from_attributes = True
+
 
 class PedidoOutSchema(BaseModel):
     id: int
@@ -25,19 +33,24 @@ class PedidoOutSchema(BaseModel):
     data_pedido: date
     itens: List[ItemPedidoOutSchema]
 
+    class Config:
+        from_attributes = True
+
+
 class ClienteOutSchema(BaseModel):
     id: int
     nome: str
     idade: int
 
+    class Config:
+        from_attributes = True
+
+
 class ClienteCreateSchema(BaseModel):
     nome: str
     idade: int
 
+
 class ClienteUpdateSchema(BaseModel):
     nome: Optional[str] = None
     idade: Optional[int] = None
-    
-
-class Config:
-        from_attributes = True  # Permite mapear de objetos SQLAlchemy
